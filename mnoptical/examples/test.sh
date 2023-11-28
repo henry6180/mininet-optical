3a02ee6511bee2aa6ccc6618011d206ce578fc0a
henry6180 <henrylin618@gmail.com>
2023-11-28 12:13:30 +0800

    revise script

--- mnoptical/examples/test.sh
+++ mnoptical/examples/test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
-#parameter
+
 directory="/home/henry/mininet-optical/examples"
 execfile="unilinear2.py"
-cmd="python3 "
+path=$directory$execfile
+sudo="sudo python3 "
 
 for boost_target_gain in 16 17 18; do
     for numAmp in 1 2 3 4 5 6 7 8 9 10; do
-        sudo $cmd $directory$execfile $boost_target_gain $numAmp<<EOF
+        $sudo $path $boost_target_gain $numAmp<<EOF
         config
         reset
         exit
